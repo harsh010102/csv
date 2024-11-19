cacda31ba5f990683920b23048097324d087eece
Rahul-7131 <razorblade7131@gmail.com>
2024-11-19 16:17:23 +0530

    fix: kernel planckster setup

--- csv_scraper.py
+++ csv_scraper.py
@@ -1,7 +1,10 @@
 import logging
 import argparse
 from app.setup import setup_csv_scraper
-from csv_scraper import scrape_csv
+from app.sdk.models import KernelPlancksterSourceData, BaseJobState
+from app.sdk.scraped_data_repository import ScrapedDataRepository
+from app.setup import setup
+from app.scraper import scrape_csv
 from app.sdk.models import ProtocolEnum
 
 def main(
@@ -9,16 +12,28 @@ def main(
     csv_url: str,
     output_path: str,
     protocol: ProtocolEnum,
+    kp_host: str,
+    kp_port: str,
+    kp_auth_token: str,
+    kp_scheme: str,
     log_level: str = "INFO"
 ) -> None:
     logger = logging.getLogger(__name__)
     logging.basicConfig(level=log_level)
 
-    scraped_data_repository = setup_csv_scraper(
-        job_id=job_id,
-        logger=logger,
-        output_path=output_path,
-        protocol=protocol
+    kernel_planckster, protocol, file_repository = setup(
+            job_id=job_id,
+            logger=logger,
+            kp_auth_token=kp_auth_token,
+            kp_host=kp_host,
+            kp_port=kp_port,
+            kp_scheme=kp_scheme,
+        )
+    
+    scraped_data_repository = ScrapedDataRepository(
+        protocol=protocol,
+        kernel_planckster=kernel_planckster,
+        file_repository=file_repository,
     )
 
     scrape_csv(
@@ -54,6 +69,34 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
+        "--kp_host",
+        type=str,
+        default="60",
+        help="kp host",
+    )
+
+    parser.add_argument(
+        "--kp_port",
+        type=int,
+        default="60",
+        help="kp port",
+    )
+
+    parser.add_argument(
+        "--kp_auth_token",
+        type=str,
+        default="60",
+        help="kp auth token",
+        )
+
+    parser.add_argument(
+        "--kp_scheme",
+        type=str,
+        default="http",
+        help="kp scheme",
+        )
+    
+    parser.add_argument(
         "--log-level",
         type=str,
         default="INFO",
@@ -67,5 +110,9 @@ if __name__ == "__main__":
         csv_url=args.csv_url,
         output_path=args.output_path,
         protocol=ProtocolEnum.S3,  # or `ProtocolEnum.LOCAL` based on environment
+        kp_host=args.kp_host,
+        kp_port=args.kp_port,
+        kp_auth_token=args.kp_auth_token,
+        kp_scheme=args.kp_scheme,
         log_level=args.log_level
     )
--- demo.sh
+++ demo.sh
@@ -3,4 +3,5 @@ python csv_scraper.py \
     --job-id 1 \
     --csv-url "https://www.uvek-gis.admin.ch/BFE/ogd/104/ogd104_stromproduktion_swissgrid.csv" \
     --output-path "./stromproduktion_swissgrid.csv" \
+    --kp_auth_token test123 --kp_host localhost --kp_port 8000 --kp_scheme http \
     --log-level INFO
